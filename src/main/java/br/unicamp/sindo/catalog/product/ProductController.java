883b4dbe7a1673d5ea964c6dcdcd65a385d55769
Gabriela Neri <gabrielapneri@gmail.com>
2018-04-06 19:02:02 -0300

    adding more attributes to product search

--- src/main/java/br/unicamp/sindo/catalog/product/ProductController.java
+++ src/main/java/br/unicamp/sindo/catalog/product/ProductController.java
@@ -27,15 +27,22 @@ public class ProductController {
 
 	@Autowired
 	ProductService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Product>> list(@RequestParam(required = false) String name
 			, @RequestParam(name="parent_product", required = false) UUID parentId
+			, @RequestParam(name="category_id", required = false) UUID categoryId
+			, @RequestParam(name="min_price", required = false) Double minPrice
+			, @RequestParam(name="max_price", required = false) Double maxPrice
+			, @RequestParam(name="tags", required = false) String tags
+			, @RequestParam(name="brand", required = false) String brand
+			, @RequestParam(name="highlight", required = false) Boolean highlight
 			, @RequestParam(defaultValue = "1") Integer page) {
 		int pageSize = 50;
-		return PageableResponseEntity.ok(service.list(name, parentId, page, pageSize+1), page, pageSize);
+		return PageableResponseEntity.ok(service.list(name, parentId, categoryId, minPrice,
+				maxPrice, tags, brand, highlight, page, pageSize + 1), page, pageSize);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> get(@PathVariable(name = "id") UUID uuid,
 			@RequestHeader(name = "ETag", required = false) String etag){
@@ -65,24 +72,24 @@ public class ProductController {
 		product = service.update(uuid, product);
 		return ETaggedResponseEntity.updated(product);
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable(name = "id") UUID uuid){
 		service.delete(uuid);
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
-	
+
 	@PostMapping(value = "/{id}:undelete")
 	public ResponseEntity<Void> undelete(@PathVariable(name = "id") UUID uuid){
 		service.undelete(uuid);
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
-	
+
 	private void validate(UUID uuid, Product product) {
 		if(product.getId() != null && !uuid.equals(product.getId())){
 			throw new BadParameterException("Path Parameter ("+uuid+") should be the same as Body Entity ("+product.getId()+")");
 		}
-		
+
 	}
 
 	private void validate(Product product) {
@@ -90,5 +97,5 @@ public class ProductController {
 			throw new BadParameterException("Should not pass ID on Product Creation");
 		}
 	}
-	
+
 }
