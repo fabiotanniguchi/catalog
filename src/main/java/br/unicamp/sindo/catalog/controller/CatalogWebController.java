3c4d55fa7d024321bcde0d88e636817520025f5e
fabiotanniguchi <ftanniguchi@gmail.com>
2018-04-26 14:29:33 -0300

    Adding dummy login

--- src/main/java/br/unicamp/sindo/catalog/controller/CatalogWebController.java
+++ src/main/java/br/unicamp/sindo/catalog/controller/CatalogWebController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CatalogWebController {
 
     private static final String HOMEPAGE = "index.html";
+    private static final String ADMIN = "admin.html";
 
     @RequestMapping("/")
     public String home(Model model) {
         return HOMEPAGE;
+    }
+
+    @RequestMapping("/admin")
+    public String admin(Model model) {
+        return ADMIN;
     }
 }
