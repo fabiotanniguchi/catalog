5454f23971349d4dfeedac3e6b3353447ac5d523
Gabriela Neri <gabrielapneri@gmail.com>
2018-05-15 23:47:24 -0300

    Create account html

---
+++ src/main/webapp/account/create.js
@@ -0,0 +1,10 @@
+var baseHost = "https://ftt-catalog.herokuapp.com/"
+
+var app = angular.module('catalogProducts');
+
+app.controller('CreateAccountCtrl', function($scope) {
+
+    $scope.createAccount = function() {
+        // TODO: submit $scope.user to back-end
+    }
+});
