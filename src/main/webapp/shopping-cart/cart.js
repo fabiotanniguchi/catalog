86be673abf4c529ffdf07a281f94e4853005fa78
Fábio Tanniguchi <ftanniguchi@gmail.com>
2018-06-08 13:11:22 -0300

    Host change for Heroku deployment

--- src/main/webapp/shopping-cart/cart.js
+++ src/main/webapp/shopping-cart/cart.js
@@ -1,4 +1,7 @@
-var baseHost = "http://localhost:8080/";
+// var baseHost = "http://localhost:8080/";
+var baseHost = "https://ftt-catalog.herokuapp.com/"
+// var baseHost = "http://produtos1-2018s1.sa-east-1.elasticbeanstalk.com/";
+
 var app = angular.module('catalogProducts');
 
 app.controller('CartCtrl', function($scope, cartService) {
