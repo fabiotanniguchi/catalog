327192d4febbd4ef7056632cf2ffa262ed13e401
Gabriel Otero <otr.gabriel@gmail.com>
2018-06-14 22:19:02 -0300

    unmocking user info

--- src/main/webapp/myaccount/infoAccount.js
+++ src/main/webapp/myaccount/infoAccount.js
@@ -2,6 +2,12 @@ var app = angular.module('catalogProducts');
 
 app.controller('InfoAccountCtrl', function($scope, $window, cartService, authService, baseHost) {
 
+	$scope.user = {}
+
+	var init = function (argument) {
+		$scope.user = authService.getLoggedUser();
+	}();
+
     $scope.logout = function(){
         console.info("logout");
         authService.logout();
